774eef0e2004734aeb1d3648c0f709b446cb6439
Bobby Ren <bobby_ren@comcast.com>
2019-06-10 13:42:16 -0400

    added incrementStarCount transaction and returns star count as message

--- functions/index.js
+++ functions/index.js
@@ -20,11 +20,38 @@ exports.giveGoldStar = functions.https.onRequest( (req, res) => {
         console.log("Invalid recipient id")
         return res.status(500).json({"message": "Invalid recipient id"})
     }
-    let ref = db.collection(`stars`).doc(toId)
-    return ref.set({
-    	count: 1
-    }).then(result => {
-    	console.log("updated stars for " + toId)
-        return res.send("updated stars for " + toId)
+
+    // working: sets to 1
+    // let ref = db.collection(`stars`).doc(toId)
+    // return ref.set({
+    // 	count: 1
+    // }).then(result => {
+    // 	console.log("updated stars for " + toId)
+    //     return res.send("updated stars for " + toId)
+    // })
+
+    return incrementStarCount(toId).then(result => {
+        console.log("giveGoldStar success with result " + JSON.stringify(result))
+        let stars = result.count
+        return res.send(`${toId} now has ${stars} stars`)
+    }).catch(err => {
+        console.log("giveGoldStar failure ", err)
+        return res.status(500).json(err)
     })
 });
+
+
+incrementStarCount = function(userId) {
+    let starsRef = db.collection('stars').doc(userId)
+    return db.runTransaction(t => {
+        return t.get(starsRef).then(doc => {
+            let newCount = doc.data().count + 1;
+            return t.update(starsRef, {count: newCount})
+        })
+    }).then(result => {
+        console.log("IncrementStarCount Transaction success")
+        return starsRef.get()
+    }).then(doc => {
+        return doc.data()
+    })
+}
